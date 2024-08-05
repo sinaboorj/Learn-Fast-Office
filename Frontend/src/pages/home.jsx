126ef0eceac56a5cbf67f4af98063183044a551d
Hossein Zarei <127826905+sinaboorj@users.noreply.github.com>
2024-08-05 15:30:43 +0330

    home page

--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -1,9 +1,26 @@
-const Home = () => {
+import homeimage from "/sysImage/home-image.jpg"
+import '../sass/home.scss'
+import { useContext } from "react";
+import { PublicContext } from "../context/publicContext";
+import logo from '/sysImage/Logo 2.png'
 
-    
+const Home = () => {
+    const { lang } = useContext(PublicContext)
     return (
         <>
             <br /><br />
+            <div className="moveing-text">
+            <img  src={logo} width={50} height={50} alt="Logo" />
+                {lang
+                    ? <h6 className="home-text">Iran National Steel Industrial Group</h6>
+                    : <h6 className="home-text">گروه ملي صنعتي فولاد ايران</h6>
+                }
+            
+            </div>
+
+            <div className='home-image'>
+                <img className="main-image" src={homeimage} alt="image" />
+            </div>
         </>
     );
 }
