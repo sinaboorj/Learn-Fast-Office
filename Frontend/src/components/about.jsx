c40ff6dcee787bf6650606a5a788e0c59d0b61ba
Hossein Zarei <127826905+sinaboorj@users.noreply.github.com>
2024-08-05 16:30:54 +0330

    About

--- Frontend/src/components/about.jsx
+++ Frontend/src/components/about.jsx
@@ -4,6 +4,8 @@ import sina from '../../public/sysImage/Sina.png'
 import { useContext } from 'react'
 import Strings from '../helper/strings';
 import { PublicContext } from '../context/publicContext';
+import { FaSkype , FaEnvelope, FaMapPin, FaLinkedin, FaInfoCircle} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const About = () => {
     const { lang } = useContext(PublicContext);
@@ -13,10 +15,15 @@ const About = () => {
             <div className="about">
                 <img className="resume-pic" src={sina} alt="Sina picture profile" />
                 <span className='resume'>Hossein Zarei (Sina)</span>
-                <div className='resume-text-fa' style={{direction : lang ? 'ltr' : 'rtl'}}>
+                <div className='resume-text-fa' style={{ direction: lang ? 'ltr' : 'rtl' }}>
                     <span>{Strings.Sina_1}</span>
                     <span>{Strings.Sina_2}</span>
                     <span>{Strings.Sina_3}</span>
+                </div>
+                <div className="contact-about">
+                    <h6 title='Email'> <FaEnvelope className="about-icon-color"  /> : sina_boorj@yahoo.com</h6>
+                    <h6 title='Skype'> <FaSkype className="about-icon-color"  /> : live:.cid.75d52dccd1467185</h6>
+                    <h6 title='LinkedIn'><a className="contact-us unlink-color unlink-decoration" href="https://www.linkedin.com/in/hossein-zarei-462a8a215/"><FaLinkedin className="about-icon-color" /> : Hossein Zarei</a></h6>
                 </div>
             </div>
         </>
--- Frontend/src/components/footer.jsx
+++ Frontend/src/components/footer.jsx
@@ -17,9 +17,7 @@ const Footer = () => {
                     
                     <footer className="footer">
                         <div className="contact">
-                            <span className="contact-us"> <FaEnvelope className="icon-color" /> : sina_boorj@yahoo.com</span>
-                            <span className="contact-us"> <FaSkype className="icon-color" /> : live:.cid.75d52dccd1467185</span>
-                            <span><a className="contact-us unlink-color unlink-decoration" href="https://www.linkedin.com/in/hossein-zarei-462a8a215/"><FaLinkedin className="icon-color" /> : Hossein Zarei</a></span>
+                            <span className="contact-us"> <FaEnvelope className="icon-color" /> : info@insig.ir</span>
                             <span className="contact-us"> <FaMapPin className="icon-color" /> : Iran.Ahvaz</span>
                             <Link className="contact-us about-sina" to='/api/about-hossein-zarei'> <FaInfoCircle className="icon-color" /> : About Programmer</Link>
                         </div>
@@ -28,7 +26,7 @@ const Footer = () => {
                                 ?
                                 <>
                                     <img className="planning-pic" src={darkLogo} alt="planning-pic" />
-                                    <h5>INSIG</h5>
+                                    <h5 style={{fontSize:'14px'}}>INSIG</h5>
                                     <span style={{ fontSize: '13px' }}>Production planning and control</span>
                                     <span style={{ fontSize: '13px' }}>Statistics monitoring and analysis</span>
                                 </>
@@ -41,7 +39,7 @@ const Footer = () => {
                         </div>
                             
                     </footer>
-                    <div className="under-footer" style={{ fontSize: '13px', color: '#383838', backgroundColor: 'white' }}>This page was programmed (Backend and Frontend) and designed by Hossein Zarei alias Sina in March 2024.</div>
+                    <div className="under-footer" style={{ fontSize: '13px', fontWeight:'600' }}>This page was programmed (Backend and Frontend) and designed by Hossein Zarei alias Sina in March 2024.</div>
                 </>
             )}
         </>
